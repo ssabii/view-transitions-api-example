b5949c8fb3df18d3089f9fcab0b445d73134eb74
ssabii <fhy1930@gmail.com>
2023-10-15 14:50:29 +0900

    feat: 이미지 도메인 등록

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['store.storeimages.cdn-apple.com'],
+  }
+}
 
 module.exports = nextConfig
